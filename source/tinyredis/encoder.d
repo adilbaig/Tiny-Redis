70d1bc22b6044eb3860ac3b01dd26db9c18a92c5
John Colvin <john.loughran.colvin@gmail.com>
2016-04-20 12:35:15 +0100

    fix for modern dmd + dub

--- source/tinyredis/encoder.d
+++ source/tinyredis/encoder.d
@@ -10,13 +10,13 @@ import std.conv : to, text;
 
 public:
 
-alias toMultiBulk encode;
+alias encode = toMultiBulk;
 
 /**
  Take an array of (w|d)string arguments and concat them to a single Multibulk
- 
+
  Examples:
- 
+
  ---
  toMultiBulk("SADD", ["fruits", "apple", "banana"]) == toMultiBulk("SADD fruits apple banana")
  ---
@@ -26,21 +26,21 @@ alias toMultiBulk encode;
 {
 	auto buffer = appender!(C[])();
 	buffer.reserve(command.length + args.length * 70); //guesstimate
-	
+
 	buffer ~= "*" ~ to!(C[])(args.length + 1) ~ "\r\n" ~ toBulk(command);
-    
+
 	foreach (c; args) {
 		buffer ~= toBulk(c);
 	}
-	
+
 	return buffer.data;
 }
 
 /**
  Take an array of varargs and concat them to a single Multibulk
- 
+
  Examples:
- 
+
  ---
  toMultiBulk("SADD", "random", 1, 1.5, 'c') == toMultiBulk("SADD random 1 1.5 c")
  ---
@@ -52,12 +52,12 @@ alias toMultiBulk encode;
     auto str = "*" ~ to!(C[])(l + 1) ~ "\r\n" ~ toBulk(command) ~ buffer.data;
     return str;
 }
- 
+
 /**
  Take an array of strings and concat them to a single Multibulk
- 
+
  Examples:
- 
+
  ---
  toMultiBulk(["SET", "name", "adil"]) == toMultiBulk("SET name adil")
  ---
@@ -66,13 +66,13 @@ alias toMultiBulk encode;
 {
 	auto buffer = appender!(C[])();
 	buffer.reserve(commands.length * 100);
-	
+
 	buffer ~= "*" ~ to!(C[])(commands.length) ~ "\r\n";
-	
+
     foreach(c; commands) {
         buffer ~= toBulk(c);
 	}
-    
+
 	return buffer.data;
 }
 
@@ -82,53 +82,53 @@ alias toMultiBulk encode;
 @trusted auto toMultiBulk(C)(const C[] command) if (isSomeChar!C)
 {
     alias command str;
-    
-	size_t 
-		start, 
+
+	size_t
+		start,
 		end,
 		bulk_count;
-	
+
 	auto buffer = appender!(C[])();
-	buffer.reserve(cast(size_t)(command.length * 1.2)); //Reserve for 20% overhead. 
-	
+	buffer.reserve(cast(size_t)(command.length * 1.2)); //Reserve for 20% overhead.
+
 	C c;
 
     for(size_t i = 0; i < str.length; i++) {
     	c = str[i];
-    	
+
     	/**
-    	 * Special support for quoted string so that command line support for 
+    	 * Special support for quoted string so that command line support for
     	 	proper use of EVAL is available.
     	*/
     	if((c == '"' || c == '\'')) {
     		start = i+1;
-			
+
 			//Circuit breaker to avoid RangeViolation
     		while(++i < str.length
     			&& (str[i] != c || (str[i] == c && str[i-1] == '\\'))
     			){}
-    		
+
 			goto MULTIBULK_PROCESS;
 		}
-    	
+
     	if(c != ' ') {
 			continue;
 		}
-    	
+
     	// c is a ' ' (space) here
     	if(i == start) {
 			start++;
 			end++;
 			continue;
 		}
-    	
+
     	MULTIBULK_PROCESS:
     	end = i;
 		buffer ~= toBulk(str[start .. end]);
 		start = end + 1;
 		bulk_count++;
     }
-	
+
 	//Nothing found? That means the string is just one Bulk
 	if(!buffer.data.length)  {
 		buffer ~= toBulk(str);
@@ -163,7 +163,7 @@ import std.array : Appender;
 @trusted uint accumulator(C, T...)(Appender!(C[]) w, T args)
 {
     uint ctr = 0;
-    
+
 	foreach (i, arg; args) {
 		static if(isSomeString!(typeof(arg))) {
 			w ~= toBulk(arg);
@@ -177,32 +177,32 @@ import std.array : Appender;
 			ctr++;
 		}
     }
-	
+
 	return ctr;
 }
 
 unittest {
-	
+
 	assert(toBulk("$2") == "$2\r\n$2\r\n");
     assert(encode("GET *2") == "*2\r\n$3\r\nGET\r\n$2\r\n*2\r\n");
     assert(encode("TTL myset") == "*2\r\n$3\r\nTTL\r\n$5\r\nmyset\r\n");
     assert(encode("TTL", "myset") == "*2\r\n$3\r\nTTL\r\n$5\r\nmyset\r\n");
-    
+
     auto lua = "return redis.call('set','foo','bar')";
     assert(encode("EVAL \"" ~ lua ~ "\" 0") == "*3\r\n$4\r\nEVAL\r\n$"~to!(string)(lua.length)~"\r\n"~lua~"\r\n$1\r\n0\r\n");
 
     assert(encode("\"" ~ lua ~ "\" \"" ~ lua ~ "\" ") == "*2\r\n$"~to!(string)(lua.length)~"\r\n"~lua~"\r\n$"~to!(string)(lua.length)~"\r\n"~lua~"\r\n");
     assert(encode("eval \"" ~ lua ~ "\" " ~ "0") == encode("eval", lua, 0));
-    
+
     assert(encode("SREM", ["myset", "$3", "$4", "two words"]) == encode("SREM myset $3 $4 'two words'"));
     assert(encode("SREM", "myset", "$3", "$4", "two words")   == encode("SREM myset $3 $4 'two words'"));
     assert(encode(["SREM", "myset", "$3", "$4", "two words"]) == encode("SREM myset $3 $4 'two words'"));
-    
+
     assert(encode("SADD", "numbers", [1,2,3]) == encode("SADD numbers 1 2 3"));
     assert(encode("SADD", "numbers", 1,2,3, [4,5]) == encode("SADD numbers 1 2 3 4 5"));
     assert(encode("TTL", "myset") == encode("TTL myset"));
-    assert(encode("TTL", "myset") == encode("TTL", ["myset"]));	
-    
+    assert(encode("TTL", "myset") == encode("TTL", ["myset"]));
+
     assert(encode("ZADD", "mysortedset", 1, "{\"a\": \"b\"}") == "*4\r\n$4\r\nZADD\r\n$11\r\nmysortedset\r\n$1\r\n1\r\n$10\r\n{\"a\": \"b\"}\r\n");
     assert(encode("ZADD", "mysortedset", "1", "{\"a\": \"b\"}") == "*4\r\n$4\r\nZADD\r\n$11\r\nmysortedset\r\n$1\r\n1\r\n$10\r\n{\"a\": \"b\"}\r\n");
 }
